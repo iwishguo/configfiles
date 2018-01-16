acc615ddc7087aa7448135bb184c7cc6342fc2f6
Heyi Guo <heyi.guo@linaro.org>
2018-01-16 13:56:43 +0800

    Add switch-gcc.sh
    The script is used to switch different versions of aarch64 gcc cross
    compiler.
    
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>

---
+++ scr/switch-gcc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#set -ex
+
+LOCATION=/usr/bin
+pushd $LOCATION
+GCC_VERSION=$1
+
+LINKS=(
+    'aarch64-linux-gnu-cpp'
+    'aarch64-linux-gnu-gcc'
+    'aarch64-linux-gnu-gcc-ar'
+    'aarch64-linux-gnu-gcc-nm'
+    'aarch64-linux-gnu-gcc-ranlib'
+    'aarch64-linux-gnu-gcov'
+    'aarch64-linux-gnu-gcov-tool'
+    )
+
+for link in ${LINKS[@]}; do
+  TARGET=${link}-$GCC_VERSION
+  if [ ! -f $TARGET ]; then
+    echo "file \"$TARGET\" does not exist!"
+    exit 1
+  fi
+  ln -sf $TARGET $link
+done
+
+popd
+
