6284329577ac9bcba6c54267370ef997587671a0
Heyi Guo <heyi.guo@linaro.org>
2018-02-07 09:39:10 +0800

    edk2-cs.sh: ignore more directories
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>

--- edk2-cs.sh
+++ edk2-cs.sh
@@ -12,8 +12,18 @@ find $CODE                         \
        -type f                     \
        ! -path "$CODE/*cscope*"    \
        ! -path "$CODE/.git*"       \
+       ! -path "$CODE/Tools/.git*"       \
+       ! -path "$CODE/HwPkg/.git*"       \
+       ! -path "$CODE/OpenPlatformPkg/.git*"       \
+       ! -path "$CODE/edk2-platforms/.git*"       \
+       ! -path "$CODE/edk2-non-osi/.git*"       \
+       ! -path "$CODE/uefi-tools/.git*"       \
+       ! -path "$CODE/TrustedFirmware*"       \
+       ! -path "$CODE/tf*"       \
+       ! -path "$CODE/EdkCompatibilityPkg*"       \
        ! -path "$CODE/Build/*"     \
        ! -path "$CODE/Conf/*"     \
+       ! -path "$CODE/CryptoPkg/Library/OpensslLib/openssl*"     \
        > $CODE/cscope.files
 
 popd
